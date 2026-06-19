419e3653daf0bd01f102f31bba29e9247233db6a
Carlos Pedroso <carlosvp8@gmail.com>
2026-06-19 11:49:49 -0300

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   schema: "prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "ts-node prisma/seeds/index.ts",
+
   },
   datasource: {
     url: env("DATABASE_URL"),
